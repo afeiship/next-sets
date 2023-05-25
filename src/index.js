082fde1359399c75cccce4e784265b7b5cd07a81
afeiship <1290657123@qq.com>
2023-05-25 21:31:40 +0800

    Release 1.0.4

--- src/index.js
+++ src/index.js
@@ -1,19 +1,18 @@
-(function () {
-  var global = typeof window !== 'undefined' ? window : this || Function('return this')();
-  var nx = global.nx || require('@jswork/next');
+import nx from '@jswork/next';
 
-  nx.sets = function (inTarget, inObject) {
-    var len = arguments.length;
-    var target = inTarget;
-    var obj = inObject;
-    len === 1 && ((target = nx), (obj = inTarget));
+nx.sets = function (inTarget, inObject) {
+  var len = arguments.length;
+  var target = inTarget;
+  var obj = inObject;
+  len === 1 && ((target = nx), (obj = inTarget));
 
-    nx.forIn(obj, function (key, value) {
-      nx.set(target, key, value);
-    });
-  };
+  nx.forIn(obj, function (key, value) {
+    nx.set(target, key, value);
+  });
+};
 
-  if (typeof module !== 'undefined' && module.exports) {
-    module.exports = nx.sets;
-  }
-})();
+if (typeof module !== 'undefined' && module.exports && typeof wx === 'undefined') {
+  module.exports = nx.sets;
+}
+
+export default nx.sets;
